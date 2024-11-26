7cfa736b2926beed4cca221b4259f8d9cbe3ace3
ajrfyd <61442171+ajrfyd@users.noreply.github.com>
2024-11-27 00:43:54 +0900

    Test: cookie

--- app/actions/login.ts
+++ app/actions/login.ts
@@ -48,7 +48,7 @@ export const login = async (_: any, formData: FormData) => {
       sameSite: "lax", // CSRF attacks cross-site request forgery attacks
       secure: process.env.NEXT_PUPLIC_ENV !== "development",
       path: "/",
-      domain: ".hkound.pe.kr",
+      // domain: ".hkound.pe.kr",
     });
 
     return {
