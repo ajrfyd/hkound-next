16300b272dbb88531ddd368f7f0905b5f54cb613
ajrfyd <61442171+ajrfyd@users.noreply.github.com>
2024-11-27 20:57:35 +0900

    Test cookie

--- app/store/socketStore.ts
+++ app/store/socketStore.ts
@@ -30,6 +30,7 @@ export const socketStore = create<State & Actions>((set, get) => ({
         env === "development"
           ? process.env.NEXT_PUBLIC_API_ENDPOINT
           : process.env.NEXT_PUBLIC_WS_ENDPOINT;
+      console.log(url);
       const socket = io(url, {
         transports: ["websocket"],
         secure: true,
