b8bebe3984d29d28f639c7aa1761b65c47cb4fec
ajrfyd <61442171+ajrfyd@users.noreply.github.com>
2024-12-06 11:58:45 +0900

    Test: set header

--- middleware.ts
+++ middleware.ts
@@ -5,21 +5,23 @@ export const middleware = async (req: NextRequest) => {
   const next = NextResponse.next();
   const cks = (await cookies()).get("rt");
 
-  if (cks) {
-    const { value } = cks;
-    (await cookies()).delete("rt");
+  req.headers.set("credentials", "include");
 
-    (await cookies()).set("rt", value, {
-      maxAge: 30 * 24 * 60 * 60, // MS
-      httpOnly: true,
-      sameSite: "none",
-      secure: true,
-      domain:
-        process.env.NEXT_PUBLIC_ENV === "development"
-          ? "localhost"
-          : process.env.NEXT_PUBLIC_COOKIE_DOMAIN,
-    });
-  }
+  // if (cks) {
+  //   const { value } = cks;
+  //   (await cookies()).delete("rt");
+
+  //   (await cookies()).set("rt", value, {
+  //     maxAge: 30 * 24 * 60 * 60, // MS
+  //     httpOnly: true,
+  //     sameSite: "none",
+  //     secure: true,
+  //     domain:
+  //       process.env.NEXT_PUBLIC_ENV === "development"
+  //         ? "localhost"
+  //         : process.env.NEXT_PUBLIC_COOKIE_DOMAIN,
+  //   });
+  // }
 
   return next;
 };
