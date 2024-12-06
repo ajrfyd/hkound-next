eb7831a3964eaaa3d3836783e188aaf7569a3e55
ajrfyd <61442171+ajrfyd@users.noreply.github.com>
2024-12-06 12:10:49 +0900

    remodify

--- middleware.ts
+++ middleware.ts
@@ -5,23 +5,23 @@ export const middleware = async (req: NextRequest) => {
   const next = NextResponse.next();
   const cks = (await cookies()).get("rt");
 
-  req.headers.set("credentials", "include");
+  // req.headers.set("credentials", "true");
 
-  // if (cks) {
-  //   const { value } = cks;
-  //   (await cookies()).delete("rt");
+  if (cks) {
+    const { value } = cks;
+    (await cookies()).delete("rt");
 
-  //   (await cookies()).set("rt", value, {
-  //     maxAge: 30 * 24 * 60 * 60, // MS
-  //     httpOnly: true,
-  //     sameSite: "none",
-  //     secure: true,
-  //     domain:
-  //       process.env.NEXT_PUBLIC_ENV === "development"
-  //         ? "localhost"
-  //         : process.env.NEXT_PUBLIC_COOKIE_DOMAIN,
-  //   });
-  // }
+    (await cookies()).set("rt", value, {
+      maxAge: 30 * 24 * 60 * 60, // MS
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+      domain:
+        process.env.NEXT_PUBLIC_ENV === "development"
+          ? "localhost"
+          : process.env.NEXT_PUBLIC_COOKIE_DOMAIN,
+    });
+  }
 
   return next;
 };
